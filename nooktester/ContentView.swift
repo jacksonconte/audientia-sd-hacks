a4be103512496e216001832b3c7c991073b66a11
stayingqold <finnr@protonmail.com>
2019-10-26 14:08:09 -0700

    Initial Commit

---
+++ nooktester/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  nooktester
+//
+//  Created by frosty on 10/26/19.
+//  Copyright © 2019 frosty. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
