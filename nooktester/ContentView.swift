6608524c47645808c6bdf336be2c98198962d669
stayingqold <finnr@protonmail.com>
2019-10-26 21:14:10 -0700

    pages kind of work

--- nooktester/ContentView.swift
+++ nooktester/ContentView.swift
@@ -6,16 +6,37 @@
 //  Copyright © 2019 frosty. All rights reserved.
 //
 
+
+// max num chars per page w/ chapter = 1500 (1493)
+
+
+
+
+
 import SwiftUI
 
-struct ContentView: View {
+struct ContentView<Page: View>: View{
+    var viewControllers: [UIHostingController<Page>]
+
+    
+    init(_ views: [Page]) {
+        self.viewControllers = views.map { UIHostingController(rootView: $0) }
+    }
+ 
+
     var body: some View {
-        Text("Hello World")
+        VStack(alignment: .leading) {
+            HStack(alignment: .top) {
+                PageViewController(controllers: viewControllers)
+            }
+            Spacer()
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        PageView(someText.map { Pages(text: $0) }).aspectRatio(3/2, contentMode: .fit)
     }
 }
